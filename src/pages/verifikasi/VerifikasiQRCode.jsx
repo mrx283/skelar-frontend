b1cc62266648d815681adf17fca98790f73503ef
Muhammad Nur Fajri <142598291+fajri900@users.noreply.github.com>
2025-08-06 17:51:08 +0700

    mengganti verifikasiqrcode

--- src/pages/verifikasi/VerifikasiQRCode.jsx
+++ src/pages/verifikasi/VerifikasiQRCode.jsx
@@ -3,9 +3,10 @@ import { useParams, useLocation } from "react-router-dom";
 import axios from "axios";
 
 const VerifikasiQRCode = () => {
+  console.log("Pathname:", location.pathname);
+  console.log("Encrypted (tanpa decodeURIComponent):", encrypted);
   const location = useLocation();
-  const encrypted = decodeURIComponent(location.pathname.replace("/verifikasi/", ""));
-  const { id } = useParams();
+  const encrypted = location.pathname.replace("/verifikasi/", "");
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [valid, setValid] = useState(false);
